1a2af0dd0be4b94b77d44e6530a2c2aca2dd9b71
Devin Ramsammy <devinram01@gmail.com>
2021-01-23 19:04:48 -0500

    add inital commits for d3 code

---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import * as d3 from "d3";
+import * as topojson from "topojson";
+
+export default class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.svg = React.createRef();
+  }
+  render() {
+    return <div></div>;
+  }
+}
