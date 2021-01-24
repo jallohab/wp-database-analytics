693007594362f4471a23566f94bf15a834cde92f
Devin Ramsammy <devinram01@gmail.com>
2021-01-24 03:27:29 -0500

    added the inital cards of the data

---
+++ src/components/Card.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
+
+export default function DataCard({ data }) {
+  return (
+    <div
+      style={{ marginLeft: "auto", marginRight: "auto", alignItems: "center" }}
+    >
+      <Container>
+        <Row>
+          {data.map((element, index) => {
+            return (
+              <Col xs={4}>
+                <Card style={{ marginBottom: "20px" }}>
+                  <Card.Body>
+                    <Card.Title>
+                      <center>{element.blurb}</center>
+                    </Card.Title>
+                    <Container>
+                      <Row>
+                        <Col>
+                          {element.source1 !== undefined ? (
+                            <Button
+                              variant="primary"
+                              href={element.source1.url}
+                            >
+                              Source 1
+                            </Button>
+                          ) : (
+                            ""
+                          )}
+                        </Col>
+                        <Col>
+                          {element.source2 !== undefined ? (
+                            <Button
+                              variant="primary"
+                              href={element.source1.url}
+                            >
+                              Source 2
+                            </Button>
+                          ) : (
+                            ""
+                          )}
+                        </Col>
+                      </Row>
+                    </Container>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+}
