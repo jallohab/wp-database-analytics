7f3bf3aef2f2dff5cce2933bc36565ac2014afb1
Devin Ramsammy <devinram01@gmail.com>
2021-01-23 19:18:28 -0500

    loaded in state data

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,7 +7,24 @@ export default class Map extends Component {
     super(props);
     this.svg = React.createRef();
   }
+  async componentDidMount() {
+    let svg = d3.select(this.svg.current);
+
+    const us = await d3.json(
+      "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json"
+    );
+    us.objects.states = {
+      type: "GeometryCollection",
+      geometries: us.objects.states.geometries.filter(function (d) {
+        return d.id < 60;
+      }),
+    };
+  }
   render() {
-    return <div></div>;
+    return (
+      <div style={{ width: "75%", marginLeft: "auto", marginRight: "auto" }}>
+        <svg ref={this.svg} viewBox="0 0 975 610"></svg>
+      </div>
+    );
   }
 }
