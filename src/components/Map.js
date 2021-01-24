26d0950e5c44b6d0beb9beb6cba9f1aefd79f0ef
Devin Ramsammy <devinram01@gmail.com>
2021-01-23 19:29:30 -0500

    add basic d3 map

--- src/components/Map.js
+++ src/components/Map.js
@@ -19,11 +19,29 @@ export default class Map extends Component {
         return d.id < 60;
       }),
     };
+
+    const projection = d3.geoAlbersUsa();
+    const path = d3.geoPath().projection(projection);
+
+    svg
+      .append("path")
+      .datum(topojson.merge(us, us.objects.states.geometries))
+      .attr("fill", "black")
+      .attr("d", path);
+
+    svg
+      .append("path")
+      .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+      .attr("fill", "none")
+      .attr("stroke", "white")
+      .attr("stroke-linejoin", "round")
+      .attr("d", path);
+    const g = svg.append("g").attr("fill", "red").attr("stroke", "black");
   }
   render() {
     return (
       <div style={{ width: "75%", marginLeft: "auto", marginRight: "auto" }}>
-        <svg ref={this.svg} viewBox="0 0 975 610"></svg>
+        <svg ref={this.svg} viewBox="0 0 1000 1000"></svg>
       </div>
     );
   }
