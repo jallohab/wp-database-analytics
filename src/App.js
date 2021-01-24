b8ba848b5be11f1d84b96fcc086f7afb2d0082c4
Abdulai <jalloha92@gmail.com>
2021-01-23 23:49:17 -0500

    The Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import NavigationBar from './components/NavigationBar'
+// eslint-disable-next-line
+import React, { Component } from 'react'; 
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <React.StrictMode>
+    <NavigationBar />
+  </React.StrictMode>
+  );
+
 }
 
 export default App;
